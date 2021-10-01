27f94ee0875cafbf4b3870387834bd68c6952353
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-10-01 14:13:44 -0700

    commit tree 55d9f55e602c8e356200e333482879bec57f626e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const userObj = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json"
+        },
+        body: JSON.stringify(userObj)
+    }
+
+    return fetch ('http://localhost:3000/users', configurationObj)
+        .then (resp => resp.json())
+        .then (data => {
+            console.log(data)
+            console.log (data.id)
+            const newP = document.createElement('p')
+            const div = document.getElementById('data-id')
+            div.append(newP)
+            newP.textContent = `The Id of the newly sent data is ${data.id}`
+        })
+        .catch ( error => {
+            alert("oh oh! Seomething went wrong")
+            const div = document.getElementById('data-id')
+            const newP = document.createElement('p')
+            div.append(newP)
+            newP.textContent = `This POST request failed: ${error.message}`
+        })
+
+        //return (fetch ('http://localhost:3000/users', configurationObj))
+}
+
